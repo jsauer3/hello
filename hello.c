311ca1624d91c95ba3b8bb51f82e067df91abb3c
Jacob Sauer <jsauer3@huskers.unl.edu>
2021-08-26 16:35:49 +0000

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,11 +1,14 @@
 /* Author: Jacob Sauer
    Date: 8/26/2021
    */
-   
+
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    
-    printf("Jacob Sauer");
+
+    printf("Jacob Sauer!\n");
+    printf("Major: Computer Science\n");
+
+    return 0;
 }
